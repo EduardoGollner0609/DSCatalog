9890af7d0bbd0d9b07e306c653fe9fbdcc564a45
EduGollner0609 <dudugollner@gmail.com>
2024-11-18 10:25:55 -0300

    Criando camada Repository.

---
+++ BackEnd/src/main/java/com/eduardo/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.eduardo.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduardo.dscatalog.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
