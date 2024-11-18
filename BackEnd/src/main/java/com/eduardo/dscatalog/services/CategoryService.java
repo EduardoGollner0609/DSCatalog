4ef5dd803c01bf5da9fbfc6444d1d87f75a27119
EduGollner0609 <dudugollner@gmail.com>
2024-11-18 11:17:56 -0300

    JPA open-in-view = false e Transactional.

--- BackEnd/src/main/java/com/eduardo/dscatalog/services/CategoryService.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eduardo.dscatalog.dto.CategoryDTO;
 import com.eduardo.dscatalog.entities.Category;
@@ -15,13 +16,16 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	@Transactional(readOnly = true)
 	public Page<CategoryDTO> findAllPaged(Pageable pageable) {
 		return repository.findAll(pageable).map(category -> new CategoryDTO(category));
 	}
 
+	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Category category = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Resource Not Found"));
 		return new CategoryDTO(category);
 	}
+
 }
