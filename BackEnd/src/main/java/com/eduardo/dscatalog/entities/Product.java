b00715e7452728c3ceb89258a1fab55347360bbf
EduGollner0609 <dudugollner@gmail.com>
2024-11-18 09:43:34 -0300

    Relacionamento Product e Category.

--- BackEnd/src/main/java/com/eduardo/dscatalog/entities/Product.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/entities/Product.java
@@ -1,12 +1,17 @@
 package com.eduardo.dscatalog.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +27,10 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
 	}
@@ -72,6 +81,14 @@ public class Product implements Serializable {
 
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void addCategory(Category category) {
+		this.categories.add(category);
 	}
 
 	@Override
