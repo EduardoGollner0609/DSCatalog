957a8d1628e3d1488537ebefa70091f050598ded
EduGollner0609 <dudugollner@gmail.com>
2024-11-18 10:35:28 -0300

    Criando método findAll (retornar todas categorias) na camada Controller.

--- BackEnd/src/main/java/com/eduardo/dscatalog/services/CategoryController.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/services/CategoryController.java
@@ -1,14 +1,24 @@
 package com.eduardo.dscatalog.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eduardo.dscatalog.dto.CategoryDTO;
+
 @RestController
-@RequestMapping
+@RequestMapping(value = "/categories")
 public class CategoryController {
 
 	@Autowired
 	private CategoryService service;
 
+	@GetMapping
+	private ResponseEntity<List<CategoryDTO>> findAll() {
+		return ResponseEntity.ok(service.findAll());
+	}
 }
