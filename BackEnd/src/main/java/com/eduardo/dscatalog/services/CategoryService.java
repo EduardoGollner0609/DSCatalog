168093422e049c0a79580be00ded20326d450fa2
EduGollner0609 <dudugollner@gmail.com>
2024-11-18 10:30:59 -0300

    Criando método findAll (retorna todas as categorias).

--- BackEnd/src/main/java/com/eduardo/dscatalog/services/CategoryService.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/services/CategoryService.java
@@ -1,8 +1,11 @@
 package com.eduardo.dscatalog.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.eduardo.dscatalog.dto.CategoryDTO;
 import com.eduardo.dscatalog.repositories.CategoryRepository;
 
 @Service
@@ -10,6 +13,8 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
-	
+
+	public List<CategoryDTO> findAll() {
+		return repository.findAll().stream().map(category -> new CategoryDTO(category)).toList();
+	}
 }
