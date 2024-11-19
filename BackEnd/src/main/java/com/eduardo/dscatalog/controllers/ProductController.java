a5ff2c3784f87a68f9d51036ffafd5fb91c53bbf
EduGollner0609 <dudugollner@gmail.com>
2024-11-19 07:36:33 -0300

    Criando método para inserir novo Produto Create/POST.

--- BackEnd/src/main/java/com/eduardo/dscatalog/controllers/ProductController.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/controllers/ProductController.java
@@ -1,13 +1,18 @@
 package com.eduardo.dscatalog.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eduardo.dscatalog.dto.ProductDTO;
 import com.eduardo.dscatalog.services.ProductService;
@@ -27,5 +32,13 @@ public class ProductController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		return ResponseEntity.ok(service.findById(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO) {
+		productDTO = service.insert(productDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(productDTO.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(productDTO);
 	}
 }
--- BackEnd/src/main/java/com/eduardo/dscatalog/services/ProductService.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/services/ProductService.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.eduardo.dscatalog.dto.CategoryDTO;
 import com.eduardo.dscatalog.dto.ProductDTO;
+import com.eduardo.dscatalog.entities.Category;
 import com.eduardo.dscatalog.entities.Product;
+import com.eduardo.dscatalog.repositories.CategoryRepository;
 import com.eduardo.dscatalog.repositories.ProductRepository;
 import com.eduardo.dscatalog.services.exceptions.ResourceNotFoundException;
 
@@ -16,6 +19,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(Pageable pageable) {
@@ -27,5 +33,26 @@ public class ProductService {
 		Product product = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Resource Not Found"));
 		return new ProductDTO(product, product.getCategories());
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO productDTO) {
+		Product product = new Product();
+		copyDtoToEntity(product, productDTO);
+		return new ProductDTO(repository.save(product));
+	}
+
+	private void copyDtoToEntity(Product product, ProductDTO productDTO) {
+		product.setName(productDTO.getName());
+		product.setPrice(productDTO.getPrice());
+		product.setImgUrl(productDTO.getImgUrl());
+		product.setDescription(productDTO.getDescription());
+		product.setDate(productDTO.getDate());
+
+
+		for (CategoryDTO categoryDTO : productDTO.getCategories()) {
+			Category category = categoryRepository.getReferenceById(categoryDTO.getId());
+			product.getCategories().add(category);
+		}
 	}
 }
