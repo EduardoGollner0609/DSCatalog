5da6312e179396209be950dd49a43df96962b923
EduGollner0609 <dudugollner@gmail.com>
2024-11-19 12:22:30 -0300

    Adicionando configurações do Authorization e Resource Server.

--- BackEnd/src/main/java/com/eduardo/dscatalog/config/AppConfig.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/config/AppConfig.java
@@ -7,13 +7,14 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
