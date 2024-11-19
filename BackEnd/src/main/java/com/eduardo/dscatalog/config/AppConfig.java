6a7711b4feedd12f8de708feaa77dbf6f6beb2b6
EduGollner0609 <dudugollner@gmail.com>
2024-11-19 11:09:27 -0300

    Implementando interfaces UserDetails e GrantedAuthority.

--- BackEnd/src/main/java/com/eduardo/dscatalog/config/AppConfig.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/config/AppConfig.java
@@ -2,9 +2,7 @@ package com.eduardo.dscatalog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
@@ -12,17 +10,6 @@ public class AppConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Configuration
-	public class SecurityConfig {
-
-		@Bean
-		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-			http.csrf(csrf -> csrf.disable());
-			http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-			return http.build();
-		}
 	}
 
 }
--- BackEnd/src/main/java/com/eduardo/dscatalog/entities/User.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/entities/User.java
@@ -1,9 +1,12 @@
 package com.eduardo.dscatalog.entities;
 
-import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -18,7 +21,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails {
 
 	private static final long serialVersionUID = 1L;
 
@@ -94,6 +97,15 @@ public class User implements Serializable {
 		roles.add(role);
 	}
 
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -109,6 +121,16 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
 	}
 
 }
