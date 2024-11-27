2b6f7f03d083ac7f1943705bbb3f838c89e96a69
EduGollner0609 <dudugollner@gmail.com>
2024-11-27 16:49:30 -0300

    Construindo end point para envio de email.

---
+++ BackEnd/src/main/java/com/eduardo/dscatalog/services/AuthService.java
@@ -0,0 +1,16 @@
+package com.eduardo.dscatalog.services;
+
+import org.springframework.stereotype.Service;
+
+import com.eduardo.dscatalog.dto.EmailDTO;
+
+import jakarta.validation.Valid;
+
+@Service
+public class AuthService {
+
+	public void createRecoverToken(@Valid EmailDTO body) {
+
+	}
+
+}
