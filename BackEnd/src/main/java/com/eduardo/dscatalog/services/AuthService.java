f8cac8e90ab391c36c3eeafba0392ca63b53ab26
EduGollner0609 <dudugollner@gmail.com>
2024-11-27 17:47:40 -0300

    Implementando lógica para envio de email.

--- BackEnd/src/main/java/com/eduardo/dscatalog/services/AuthService.java
+++ BackEnd/src/main/java/com/eduardo/dscatalog/services/AuthService.java
@@ -1,16 +1,56 @@
 package com.eduardo.dscatalog.services;
 
+import java.time.Instant;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eduardo.dscatalog.dto.EmailDTO;
-
-import jakarta.validation.Valid;
+import com.eduardo.dscatalog.entities.PasswordRecover;
+import com.eduardo.dscatalog.entities.User;
+import com.eduardo.dscatalog.repositories.PasswordRecoverRepository;
+import com.eduardo.dscatalog.repositories.UserRepository;
+import com.eduardo.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class AuthService {
 
-	public void createRecoverToken(@Valid EmailDTO body) {
+	@Value("${email.password-recover.token.minutes}")
+	private Long tokenMinutes;
 
+	@Value("${email.password-recover.uri}")
+	private String recoverUri;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordRecoverRepository passwordRecoverRepository;
+
+	@Autowired
+	private EmailService emailService;
+
+	@Transactional
+	public void createRecoverToken(EmailDTO body) {
+		User user = userRepository.findByEmail(body.getEmail());
+		if (user == null) {
+			throw new ResourceNotFoundException("Email não encontrado.");
+		}
+
+		String token = UUID.randomUUID().toString();
+
+		PasswordRecover entity = new PasswordRecover();
+		entity.setEmail(body.getEmail());
+		entity.setToken(token);
+		entity.setExpiration(Instant.now().plusSeconds(tokenMinutes * 60L));
+		entity = passwordRecoverRepository.save(entity);
+
+		String text = "Acesse o link para definir uma nova senha\n\n" + recoverUri + token;
+
+		emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
 	}
 
 }
